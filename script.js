4d8222a58ef630df59b04d67bdb9da8135183590
RAM_MATRIX <phines406@gmail.com>
2026-03-10 22:30:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const pi =
 "3.14159265358979323846264338327950288419716939937510"+
 "58209749445923078164062862089986280348253421170679"+
-"82148086513282306647093844609550582231725359408128";
+"82148086513282306647093844609550582231725359408128"+
+"48111745028410270193852110555964462294895493038196";
 
-const text = document.getElementById("piText");
+const textPath = document.getElementById("piText");
 
-text.textContent = pi.repeat(20);
+textPath.textContent = pi.repeat(20);
 
 let offset = 0;
 
-setInterval(()=>{
+function animate(){
 
-offset += 0.08;
+offset += 0.05;
 
-text.setAttribute("startOffset", offset + "%");
+if(offset > 100){
+offset = 0;
+}
 
-},40);
+textPath.setAttribute("startOffset", offset + "%");
+
+requestAnimationFrame(animate);
+
+}
+
+animate();
