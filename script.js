c63c68934d6c7cb7f0e1649462a88cb7952baf47
RAM_MATRIX <phines406@gmail.com>
2026-03-10 22:28:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const pi =
 "3.14159265358979323846264338327950288419716939937510"+
-"58209749445923078164062862089986280348253421170679";
+"58209749445923078164062862089986280348253421170679"+
+"82148086513282306647093844609550582231725359408128";
 
 const text = document.getElementById("piText");
 
-text.textContent = pi.repeat(10);
+text.textContent = pi.repeat(20);
 
-// движение цифр по линии
 let offset = 0;
 
 setInterval(()=>{
-offset += 0.1;
+
+offset += 0.08;
+
 text.setAttribute("startOffset", offset + "%");
+
 },40);
